17520476f418b586831762787da6e8ade8ddd3dd
TAMANPREET13 <61380869+TAMANPREET13@users.noreply.github.com>
2022-03-26 18:10:02 +0530

    Revert "Optimized the code readability in script.js"

--- LightDarkMode/script.js
+++ LightDarkMode/script.js
@@ -1,53 +1,62 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
-const nav = document.getElementById("nav");
-const toggleIcon = document.getElementById("toggle-icon");
-const image1 = document.getElementById("image1");
-const image2 = document.getElementById("image2");
-const image3 = document.getElementById("image3");
-const textBox = document.getElementById("text-box");
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const textBox = document.getElementById('text-box');
 
-const ToggleLightDarkMode = (isDark) => {
-  nav.style.backgroundColor = isDark
-    ? "rgb(0 0 0 / 50%)"
-    : "rgb(255 255 255 / 50%)";
 
-  textBox.style.backgroundColor = isDark
-    ? "rgb(255 255 255 / 50%)"
-    : "rgb(0 0 0 / 50%)";
+//Dark or light images
+function imageMode(color) {
+    image1.src = `img/undraw_proud_coder_${color}.svg`;
+    image2.src = `img/undraw_feeling_proud_${color}.svg`;
+    image3.src = `img/undraw_conceptual_idea_${color}.svg`;
+}
 
-  toggleIcon.children[0].textContent = isDark ? "Dark Mode" : "Light Mode";
+//Dark MOde Style
+function darkMode() {
+    nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+    textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+    toggleIcon.children[0].textContent = 'Dark Mode';
+    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+    imageMode('dark');
+}
 
-  isDark
-    ? toggleIcon.children[1].classList.replace("fa-sun", "fa-moon")
-    : toggleIcon.children[1].classList.replace("fa-moon", "fa-sun");
+//Light MOde Style
+function lightMode() {
+    nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+    textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+    toggleIcon.children[0].textContent = 'Light Mode';
+    toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+    imageMode('light');
+}
 
-  isDark ? imageMode("dark") : imageMode("light");
-};
 
-// Switch theme dynamically
-const switchTheme = (event) => {
-  if (event.target.checked) {
-    document.documentElement.setAttribute("data-theme", "dark");
-    localStorage.setItem("theme", "dark");
-    ToggleLightDarkMode(true);
-  } else {
-    document.documentElement.setAttribute("data-theme", "light");
-    localStorage.setItem("theme", "light");
-    ToggleLightDarkMode(false);
-  }
-};
 
-// We will use the onchange event instead of a click event
-toggleSwitch.addEventListener("change", switchTheme);
+//Switch theme dynamicallly
+function switchTheme(event) {
+    if (event.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+        darkMode();
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+        lightMode();
+    }
+}
 
-// Check Local Storage for theme
-const currentTheme = localStorage.getItem("theme");
+//Event listeners
+toggleSwitch.addEventListener('change', switchTheme);
 
-if (currentTheme) {
-  document.documentElement.setAttribute("data-theme", currentTheme);
+//Check local storage for theme
+const currTheme = localStorage.getItem('theme');
+if (currTheme) {
+    document.documentElement.setAttribute('data-theme', currTheme);
 
-  if (currentTheme === "dark") {
-    toggleSwitch.checked = true;
-    ToggleLightDarkMode(true);
-  }
+    if (currTheme === 'dark') {
+        toggleSwitch.checked = true;
+        darkMode();
+    }
 }
